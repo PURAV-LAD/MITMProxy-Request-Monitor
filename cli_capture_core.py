b026d38ced666c7db0dc179175fdfb61247272ba
PURAV LAD <107126701+PURAV-LAD@users.noreply.github.com>
2025-08-09 19:13:51 +0530

    Create cli_capture_core.py

---
+++ cli_capture_core.py
@@ -0,0 +1,9 @@
+# Download Certificate From the http://mitm.it/ after applying proxy to localhost with port 8080  in windows proxy Setting; 127.0.0.1:8080
+# mitmproxy -s capture.py
+import tkinter as tk
+from mitmproxy import http
+
+def request(flow: http.HTTPFlow) -> None:
+    print(f"Request URL: {flow.request.url}")
+    print(f"Request Headers: {flow.request.headers}")
+    print(f"Request Body: {flow.request.content}")
